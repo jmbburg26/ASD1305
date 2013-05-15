480723b322626f00793a9236b976be6a6d182520
John Brandenburg <jmbburg26@fullsail.edu>
2013-05-14 23:38:46 -0400

    FIxed function to add random key to store data function.

--- Projects/js/main.js
+++ Projects/js/main.js
@@ -1,10 +1,8 @@
 //Save data from form into local storage function
 $('#submit').on('click', function (key){
-    if(!key){
-      var userValues    = Math.floor(Math.random()*100000001);
-    }else{
-      userValues = key;
-    }
+      
+    var key    = Math.floor(Math.random()*100000001);
+
     var userData = {};
         userData.fname = $('#fname').val();
         userData.lname = $('#lname').val();
@@ -13,40 +11,40 @@ $('#submit').on('click', function (key){
         userData.datedue = $('#datedue').val();
         userData.notes = $('#notes').val();
 
-    localStorage.setItem(userValues, JSON.stringify(userData));
+    localStorage.setItem(key, JSON.stringify(userData));
     alert("Homework Added!");
     console.log(userData);
 });
 
 //Display data from local storage
-$("#view").on('pageinit', function(){
+$('#view').on('pageinit', function(){
         $("#savedList").empty();
         for (var i= 0, j=localStorage.length; i<j ; i++){
             var key = localStorage.key(i);
             var item = JSON.parse(localStorage.getItem(key));
-            //console.log(item);
-            var makeSubList = $("<li></li>");
-            var makeSubLi = $( "<h3>"+item.fname[1]+"</h3>"+
-                "<p><strong>"+item.lname[1]+"</strong></p>"+
-                "<p>"+item.email[1]+"</p>" +
-                "<p>"+item.subject[1]+"</p>" +
-                "<p>"+item.datedue[1]+"</p>" +
-                "<p>"+item.notes[1]+"</p>" );
+            console.log(item);
+            var makeSubList = $("<div></div>");
+            var makeSubLi = $( "<h3>"+item.datedue[1].val+"</h3>"+
+                "<p>"+item.fname[1].val+"</p>" +
+                "<p>"+item.lname[1].val+"</p>"+
+                "<p>"+item.email[1].val+"</p>" +
+                "<p>"+item.subject[1].val+"</p>" +
+                "<p>"+item.notes[1].val+"</p>" );
             var makeLink = $("<a href='#' id='"+key+"'>Edit</a>");
             makeLink.on('click', function(){
-                console.log("This is my key: "+this.id);
+               // console.log("This is my key: "+this.id);
             });
             makeLink.html(makeSubLi);
             makeSubList.append(makeLink).appendTo("#savedList");
         }; // end for loop
-        //$("ul").listview('refresh');
-    });
+        $("ul").listview('refresh');
+    });  // end storage.on
 
 //Function to add json data 
 $('#loadjson').on('click', function(){
     if(localStorage.length === 0){
       alert("There are no assignments in Local Storage so default data has been added.");
-      defaultData();
+      //defaultData();
     }
 });
 
