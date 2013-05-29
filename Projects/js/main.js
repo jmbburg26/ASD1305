3cdd157dd1bb51aa5038f093644cd04f2dc54fe5
John Brandenburg <jmbburg26@fullsail.edu>
2013-05-29 13:18:33 -0400

    Added local validate script

--- Projects/js/main.js
+++ Projects/js/main.js
@@ -1,31 +1,49 @@
-$(document).on('pageinit', '#home', '#assignments', function(){
+//Display data from CouchDB as a listview with links to individual pages
+$('#view').on('pageinit', function(){
+	$.couch.db("asd1305project").view("asd1305app/assignments", {
+		success: function(data){
+			//console.log(data);
+			$('#savedList').empty();
+			$.each(data.rows, function(index, assignments){
+					//console.log(assignments);
+					var fname = assignments.value.fname;
+					var lname = assignments.value.lname;
+					var email = assignments.value.email;
+					var subject = assignments.value.subject;
+					var datedue = assignments.value.date;
+					var notes = assignments.value.notes;
+					//console.log(datedue);
+					$('#savedList').append(
+						$('<li>').append(
+							$('<a>').attr("href", "assignments.html?assignments=" + assignments.value.subject)
+								.text(subject + ": " + datedue)
+						)
+					);
+				});
+				$('#savedList').listview('refresh');
+		}
+	});
+});	
 
-	//Display data from CouchDB as a listview with links to individual pages
-	$('#view').on('pageinit', function(){
-		$.couch.db("asd1305project").view("asd1305app/assignments", {
-			success: function(data){
-				//console.log(data);
-				$('#savedList').empty();
-				$.each(data.rows, function(index, assignments){
-						//console.log(assignments);
-						var fname = assignments.value.fname;
-						var lname = assignments.value.lname;
-						var email = assignments.value.email;
-						var subject = assignments.value.subject;
-						var datedue = assignments.value.date;
-						var notes = assignments.value.notes;
-						//console.log(datedue);
-						$('#savedList').append(
-							$('<li>').append(
-								$('<a>').attr("href", "assignments.html?assignments=" + assignments.value.date)
-									.text(subject + ": " + datedue)
-							)
-						);
-					});
-					$('#savedList').listview('refresh');
-			}
-		});
-	});	
+var urlVars = function(){
+	var urlData = $($.mobile.activePage).data("url");
+	var urlParts = urlData.split('?');
+	var urlPairs = urlParts[1].split('&');
+	var urlValues = {};
+	for (var pair in urlPairs){
+		var keyValue = urlPairs[pair].split('=');
+		var key = decodeURIComponent(keyValue[0]);
+		var value = decodeURIComponent(keyValue[1]);
+		urlValues[key] = value;
+		}
+		return urlValues;
+		//console.log(urlValues);
+};
+
+$(document).on('pageinit', '#assignments', function(){
+	var assignment = urlVars()["assignment"];
+	//var urlData = $(this).data("url"):
+	console.log(assignment);		
 });
 
 //Save data from form into CouchDB function call
